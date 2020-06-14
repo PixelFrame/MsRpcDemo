8218f6481921387d2aa4fac9000576f72fab8c94
Pixel Frame <pm421@live.com>
2020-06-14 16:55:30 +0800

    添加项目文件。

---
+++ RpcClientTcp/RpcClientTcp.cpp
@@ -0,0 +1,104 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include "..\MsRpcDemo\RpcDemo_h.h"
+
+#pragma comment(lib, "Rpcrt4.lib")
+
+void PrintRpcStatus(const char* function, RPC_STATUS status)
+{
+    std::cout << "Entering: " << std::left << std::setw(25) << function
+        << "-> RPC Status: " << status << std::endl;
+    if (status)
+    {
+        std::cout << "Exiting on error..." << std::endl;
+        exit(status);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    RPC_STATUS status;
+    RPC_WSTR pszUuid = NULL;
+    RPC_WSTR pszProtocolSequence = (RPC_WSTR)L"ncacn_ip_tcp";
+    RPC_WSTR pszNetworkAddress = NULL;
+    RPC_WSTR pszEndpoint = NULL;
+    RPC_WSTR pszOptions = NULL;
+    RPC_WSTR pszStringBinding = NULL;
+    RPC_WSTR pszString = (RPC_WSTR)L"hello, world";
+    unsigned long ulCode;
+
+    if (argc != 3)
+    {
+        std::cout << "Usage: " << argv[0] << " <Server Name> <Hello Text>" << std::endl;
+        return 1;
+    }
+
+    if (strcmp(argv[1], ".") == 0)
+    {
+        status = RpcStringBindingCompose(pszUuid,
+            pszProtocolSequence,
+            pszNetworkAddress,
+            pszEndpoint,
+            pszOptions,
+            &pszStringBinding);
+        PrintRpcStatus("RpcStringBindingCompose", status);
+    }
+    else
+    {
+        wchar_t netAddr[255] = L"\\\\";
+        int wchars_num = MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, NULL, 0);
+        wchar_t* wargv = new wchar_t[wchars_num];
+        MultiByteToWideChar(CP_UTF8, 0, argv[1], -1, wargv, wchars_num);
+
+        wcscat_s(netAddr, 255, wargv);
+        delete[] wargv;
+        pszNetworkAddress = (RPC_WSTR)netAddr;
+
+        status = RpcStringBindingCompose(pszUuid,
+            pszProtocolSequence,
+            pszNetworkAddress,
+            pszEndpoint,
+            pszOptions,
+            &pszStringBinding);
+        PrintRpcStatus("RpcStringBindingCompose", status);
+        std::cout << "Connecting \\\\" << argv[1] << std::endl;
+    }
+    status = RpcBindingFromStringBinding(pszStringBinding, &RpcDemo_Binding);
+    PrintRpcStatus("RpcBindingFromStringBinding", status);
+
+    RpcTryExcept
+    {
+        if (strcmp(argv[2], "SHUTDOWN") == 0)
+        {
+            Shutdown();
+        }
+        else
+        {
+            Hello((unsigned char*)argv[2]);
+        }
+    }
+        RpcExcept(1)
+    {
+        ulCode = RpcExceptionCode();
+        std::cout << "RPC Exception: " << ulCode << std::endl;
+    }
+    RpcEndExcept;
+
+    status = RpcStringFree(&pszStringBinding);
+    PrintRpcStatus("RpcStringFree", status);
+    status = RpcBindingFree(&RpcDemo_Binding);
+    PrintRpcStatus("RpcBindingFree", status);
+
+    return 0;
+}
+
+void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+{
+    return (malloc(len));
+}
+
+void __RPC_USER midl_user_free(void __RPC_FAR* ptr)
+{
+    free(ptr);
+}
---
+++ RpcServerTcp/RpcServerTcp.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#include "..\MsRpcDemo\RpcDemo_h.h"
+
+#pragma comment(lib, "Rpcrt4.lib")
+
+void PrintRpcStatus(const char* function, RPC_STATUS status)
+{
+    std::cout << "Entering: " << std::left << std::setw(25) << function
+        << "-> RPC Status: " << status << std::endl;
+    if (status)
+    {
+        std::cout << "Exiting on error..." << std::endl;
+        exit(status);
+    }
+}
+
+void Hello(const unsigned char* psz)
+{
+    std::cout << "Get Message From Client: " << psz << std::endl;
+}
+
+void Shutdown(void)
+{
+    RPC_STATUS status;
+
+    status = RpcMgmtStopServerListening(NULL);
+    PrintRpcStatus("RpcMgmtStopServerListening", status);
+
+    status = RpcServerUnregisterIf(NULL, NULL, FALSE);
+    PrintRpcStatus("RpcServerUnregisterIf", status);
+}
+
+int main(int argc, char* argv[])
+{
+    RPC_STATUS status;
+    RPC_WSTR pszProtocolSequence = (RPC_WSTR)L"ncacn_ip_tcp";
+    RPC_WSTR pszSecurity = NULL;
+    RPC_BINDING_VECTOR* rpcBindingVector;
+    unsigned int cMinCalls = 1;
+
+    status = RpcServerUseProtseq(pszProtocolSequence, 20, pszSecurity);
+    PrintRpcStatus("RpcServerUseProtseq", status);
+
+    status = RpcServerInqBindings(&rpcBindingVector);
+    PrintRpcStatus("RpcServerInqBindings", status);
+
+    status = RpcEpRegister(RpcDemo_v1_0_s_ifspec, rpcBindingVector, NULL, NULL);
+    PrintRpcStatus("RpcEpRegister", status);
+
+    status = RpcServerRegisterIf(RpcDemo_v1_0_s_ifspec, NULL, NULL);
+    PrintRpcStatus("RpcServerRegisterIf", status);
+
+    status = RpcServerListen(cMinCalls, 20, FALSE);
+    if (status) PrintRpcStatus("RpcServerListen", status);
+
+    return 0;
+}
+
+void __RPC_FAR* __RPC_USER midl_user_allocate(size_t len)
+{
+    return (malloc(len));
+}
+
+void __RPC_USER midl_user_free(void __RPC_FAR* ptr)
+{
+    free(ptr);
+}
